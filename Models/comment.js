b768ac4dd97c0b5c8bc9a582c929e362e87a8704
Mohammed Alrabai <m.alrabai4@gmail.com>
2023-06-16 17:09:16 +0300

    add dev to main

--- Models/comment.js
+++ Models/comment.js
@@ -13,7 +13,7 @@ const commentSchema = new mongoose.Schema({
     agreeCounter: {
         type: Number,
     },
-    // Relationship Here
+    // Relationship Here 
     EmployeeAuther:{
        type: mongoose.Schema.Types.ObjectId,
        ref:"Employee"
