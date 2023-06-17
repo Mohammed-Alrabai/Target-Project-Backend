f64a9bb447d74843eb8bb5f6265ac8168cfcd41e
Mohammed Alrabai <m.alrabai4@gmail.com>
2023-06-17 04:17:41 +0300

    for spase -_-

--- Controller/employee.js
+++ Controller/employee.js
@@ -3,117 +3,129 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 const bcrypt = require("bcrypt");
-const Challenge = require('../Models/challeng.js')
-const Comment = require('../Models/comment.js')
-const Employee = require('../Models/employee')
-const saltRounds = Number(process.env.saltRounds)
+const Challenge = require("../Models/challeng.js");
+const Comment = require("../Models/comment.js");
+const Employee = require("../Models/employee");
+const saltRounds = Number(process.env.saltRounds);
 //create employee
-exports.CreateEmployee = async (req,res)=>{
-const username = "lina";
-const password = "lina12345";
-const passHash = await bcrypt.hash(password,saltRounds );
+exports.CreateEmployee = async (req, res) => {
+  const username = "mohammad";
+  const password = "moh123";
+  const passHash = await bcrypt.hash(password, saltRounds);
 
-const newEmployee = new Employee({
-  username:username,
-  password:passHash
-})
-newEmployee.save().then((newEmp)=>{
-   res.status(200).json(newEmp);
-}).catch((err) => {
+  const newEmployee = new Employee({
+    username: username,
+    password: passHash,
+  });
+  newEmployee
+    .save()
+    .then((newEmp) => {
+      res.status(200).json({
+        result: newEmp,
+      });
+    })
+    .catch((err) => {
       res.json(err);
     });
-}
-
+};
 
 //employee Login
-exports.EmployeeLogin = (req,res)=>{
-const username = req.body.username;
-const password = req.body.password;
-console.log(username + password)
-Employee.findOne({ username: username })
+exports.EmployeeLogin = (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  Employee.findOne({ username: username })
     .select("+password")
     .then(async (result) => {
-      console.log(result)
- const hashedPass = result.password;
- const compare = await bcrypt.compare(password, hashedPass);
-    if (compare) {
+      console.log(result);
+      const hashedPass = result.password;
+      const compare = await bcrypt.compare(password, hashedPass);
+      if (compare) {
         const token = jwt.sign({ result }, process.env.JWT_SECRET, {
           expiresIn: "1h",
         });
-        console.log(token)
-         res.status(200).json({ token: token });
+        res.status(200).json({ token: token });
       }
-       }).catch((error) => {
+    })
+    .catch((error) => {
       res.json(error);
     });
-}
+};
 
 //to view AllChallanges
-exports.ChallangeList = (req,res)=>{
-Challenge.find().then((challangeList)=>{
-res.status(200).json({
-    result:challangeList
-})
-}).catch((err)=>{
-res.status(500).json({
-  message: err,
-})
-})
-}
+exports.ChallangeList = (req, res) => {
+  Challenge.find()
+    .then((challangeList) => {
+      res.status(200).json({
+        result: challangeList,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: err,
+      });
+    });
+};
 
 // get challenge by id
-exports.ChallengeById = (req,res)=>{
-const ChallengId  = req.params.id
-console.log(ChallengId)
-Challenge.findById(ChallengId).then((theChallengResult)=>{
-  console.log(theChallengResult)
-  res.status(200).json({
-   result: theChallengResult
-  })
-}).catch((error)=>{
-  res.status(500).json({
-  message: error,
-})
-})
-}
+exports.ChallengeById = (req, res) => {
+  const ChallengId = req.params.id;
+  console.log(ChallengId);
+  Challenge.findById(ChallengId)
+    .then((theChallengResult) => {
+      console.log(theChallengResult);
+      res.status(200).json({
+        result: theChallengResult,
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: error,
+      });
+    });
+};
 
 // get challenge by id
-exports.Comment = (req,res)=>{
-const ChallengId  = req.params.id
-console.log(ChallengId)
-const inputbody = req.body.inputbody
-const emp = res.locals.decoded;
-console.log(emp)
-const empId = emp.result._id;
+exports.Comment = (req, res) => {
+  const ChallengId = req.params.id;
+  console.log(ChallengId);
+  const inputbody = req.body.inputbody;
+  const emp = res.locals.decoded;
+  console.log(emp);
+  const empId = emp.result._id;
 
-const newComment = new Comment({
-  body:inputbody,
-  EmployeeAuther:ChallengId
-})
+  const newComment = new Comment({
+    body: inputbody,
+    EmployeeAuther: ChallengId,
+  });
 
-newComment.save().then((newCommentResult)=>{
-///adding comment to the employee model (relationship)
+  newComment
+    .save()
+    .then((newCommentResult) => {
+      ///adding comment to the employee model (relationship)
 
-Employee.findById(empId).then((employee)=>{
-  comments = newComment_id
-  employee.save().then((savedComment)=>{
-     res.status(200).json({
-   result: savedComment
-  })
-  })
-}).catch((error)=>{
-  res.status(500).json({
-  message: error,
-})
-})
+      Employee.findById(empId)
+        .then((employee) => {
+          comments = newComment_id;
+          employee.save().then((savedComment) => {
+            res.status(200).json({
+              result: savedComment,
+            });
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+          res.status(500).json({
+            message: error,
+          });
+        });
 
-  res.status(200).json({
-   result: newCommentResult
-  })
-}).catch((error)=>{
-  res.status(500).json({
-  message: error,
-})
-})
-}
-
+      res.status(200).json({
+        result: newCommentResult,
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: error,
+      });
+    });
+};
--- Router/employeeRouter.js
+++ Router/employeeRouter.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const employeeController = require('../Controller/employee')
-const {verifyToken} =require('../Middleware/authentication')
+const {verifyToken , checkLogin} =require('../Middleware/authentication')
 
-router.get("/challengeList",employeeController.ChallangeList)
+router.get("/challengeList", checkLogin, employeeController.ChallangeList);
 router.get("/ChallengeById/:id",employeeController.ChallengeById)
 router.post("/CreateEmp",employeeController.CreateEmployee)
-router.post("/emplogin",employeeController.EmployeeLogin)
+router.post("/emplogin", employeeController.EmployeeLogin);
 router.post("/Comment/:id",verifyToken,employeeController.Comment)
 
 
